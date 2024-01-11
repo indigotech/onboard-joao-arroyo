3f40d870bef7c3583f4b0e2ea1836e344c4e906e
JoaoArroyo004 <comp.arroyo@gmail.com>
2024-01-11 17:28:53 -0300

    create login request helper

--- src/test/login.test.ts
+++ src/test/login.test.ts
@@ -1,30 +1,16 @@
-import axios from 'axios';
 import { expect } from 'chai';
 import { User } from '../entity/User';
+import { loginRequest } from './helper';
 
 describe('Login Mutation', () => {
   it('should return mocked data in the specified format', async () => {
     const params = { email: 'mocked@fake.com', password: 'v4l1dp4ss' };
-    const result = await axios.post(`http://localhost:${process.env.PORT}/graphql`, {
-      query: `mutation ($input: LoginInput!) {
-    login(data: $input) {          
-      user {
-        id
-        name
-        email
-        birthDate
-      }
-      token    
-    }
-  }`,
-      variables: {
-        input: params,
-      },
-    });
+    const result = await loginRequest({ input: params });
 
     expect(result).to.exist;
 
     const unwrappedResponse: { user: User; token: string } = result?.data?.data?.login;
+
     expect(unwrappedResponse.token).to.be.a('string');
     expect(unwrappedResponse.user.id).to.match(/^\d+$/);
     expect(unwrappedResponse.user.email).to.equal(params.email);
