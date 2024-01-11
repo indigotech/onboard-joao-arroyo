b0c0f5429b47e752210fc7345f1732d638fadf3d
JoaoArroyo004 <comp.arroyo@gmail.com>
2024-01-11 10:02:15 -0300

    remove unnecessary queries & add email test case

--- src/test/create-user.test.ts
+++ src/test/create-user.test.ts
@@ -10,18 +10,10 @@ describe('Create User Mutation', () => {
   });
 
   it('should return no users at the beginning', async () => {
-    const result = await axios.post(`http://localhost:${process.env.PORT}/graphql`, {
-      query: `query {
-            users {
-              id
-              name
-              email
-              birthDate
-            }
-          }`,
-    });
+    const userRepository = appDataSource.getRepository(User);
+    const result = await userRepository.count();
 
-    expect(result.data?.data.users).to.be.empty;
+    expect(result).to.equal(0);
   });
 
   it('should not register an user due to a weak password', async () => {
@@ -46,7 +38,7 @@ describe('Create User Mutation', () => {
       },
     });
 
-    expect(result.data?.errors[0].message).to.equal(
+    expect(result?.data?.errors[0].message).to.equal(
       'Password is not strong enough: should be at least 6 characters long and have at least one letter and 1 digit.',
     );
   });
@@ -84,33 +76,55 @@ describe('Create User Mutation', () => {
     expect(createdUser.birthDate).to.equal(newUser.birthDate);
     expect(createdUser.name).to.equal(newUser.name);
 
-    const fetchedResult = await axios.post(`http://localhost:${process.env.PORT}/graphql`, {
-      query: `query FindUser($findUserId: ID!) {
-        findUser(id: $findUserId) {
-            birthDate
-            name
-            id
-            email
-            password
-        }
-        }`,
-      variables: {
-        findUserId: createdUser.id,
+    const userRepository = appDataSource.getRepository(User);
+
+    const fetchedUser = await userRepository.findOne({
+      where: {
+        id: createdUser.id,
       },
     });
 
-    expect(fetchedResult.data?.data?.findUser).to.exist;
-
-    const fetchedUser: User = fetchedResult.data?.data?.findUser;
+    expect(fetchedUser).to.exist;
 
     // check that the object returned from database query is the same as the initially assigned
-    expect(fetchedUser.id).to.exist;
-    expect(fetchedUser.id).to.match(/^\d+$/);
-    expect(fetchedUser.email).to.equal(newUser.email);
-    expect(fetchedUser.birthDate).to.equal(newUser.birthDate);
-    expect(fetchedUser.name).to.equal(newUser.name);
-    expect(fetchedUser.password).to.have.string('$2b$10$');
-    const matchedPasswords = await bcrypt.compare(newUser.password, fetchedUser.password);
+
+    expect(fetchedUser?.id).to.exist;
+    expect(fetchedUser?.id).to.match(/^\d+$/);
+    expect(fetchedUser?.email).to.equal(newUser.email);
+    expect(fetchedUser?.birthDate).to.equal(newUser.birthDate);
+    expect(fetchedUser?.name).to.equal(newUser.name);
+    expect(fetchedUser?.password).to.have.string('$2b$10$');
+
+    const hashedPassword = fetchedUser?.password ?? '';
+    const matchedPasswords = await bcrypt.compare(newUser.password, hashedPassword);
     expect(matchedPasswords).to.be.true;
+  });
+
+  it('should not register an user due to repeated email', async () => {
+    const newUser = {
+      password: 'v4lidp4ass',
+      email: 'first@example.com',
+      birthDate: '09/12/2004',
+      name: 'user 2',
+    };
+
+    const result = await axios.post(`http://localhost:${process.env.PORT}/graphql`, {
+      query: `mutation ($input: UserInput!) {
+    createUser(data: $input) {
+      email
+      birthDate
+      name
+      id
+    }
+  }`,
+      variables: {
+        input: newUser,
+      },
+    });
+
+    expect(result?.data?.errors[0].message).to.equal('Email already in use.');
+
+    const userRepository = appDataSource.getRepository(User);
+    expect(await userRepository.count()).to.equal(1);
   });
 });
